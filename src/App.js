b123c34d5e3f76106763aecc4107340a6c5bc4f1
Min Kookie <min03041999@gmail.com>
2022-06-24 10:27:35 +0700

    add coding

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Tabs from "./Components/TabComponent/Tab";
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TabComponent/Tab.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import FirstTab from "../AllTabs/FirstTab";
+import SecondTab from "../AllTabs/SecondTab";
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState("tab1");
+
+  const handleTab1 = () => {
+    setActiveTab("tab1");
+  };
+
+  const handleTab2 = () => {
+    setActiveTab("tab2");
+  };
+
+  return (
+    <div className="Tabs">
+      <ul className="nav">
+        <li
+          className={activeTab === "tab1" ? "active" : ""}
+          onClick={handleTab1}
+        >
+          Tab1
+        </li>
+        <li
+          className={activeTab === "tab2" ? "active" : ""}
+          onClick={handleTab2}
+        >
+          Tab2
+        </li>
+      </ul>
+      <div className="outlet">
+        {activeTab === "tab1" ? <FirstTab /> : <SecondTab />}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
